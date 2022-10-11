e36ac04af5d873b31add69a62ee4bde004d0b907
EZ586 <ezhangw@gmail.com>
2022-10-11 14:07:25 -0400

    Added Message Page to Website

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import './Messages.css';
 import Home from './Views/Home.js';
 // import Contact from './Views/Contact.js';
 // import Error from './Views/Error.js';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Messages1 from './Views/Messages1.js';
 
 
 function App() {
@@ -11,6 +13,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Messages" element = {<Messages1/>}></Route>
         {/* <Route path="/Contact" element={<Contact />} /> */}
         {/* <Route path="/*" element={<Error />} /> */}
       </Routes>
---
+++ frontend/src/Views/Messages1.js
@@ -0,0 +1,14 @@
+function Messages1(props) {
+    return (
+        <div>
+            <h2>Messages</h2>
+            <div className="gridLayout">
+                <div className="People">People</div>
+                <div className="Text">Text Messages</div>
+            </div>
+            <div className="ToHome"><a href="/">To Home</a></div>
+        </div>
+    );
+}
+
+export default Messages1;
