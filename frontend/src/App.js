fdd574c3d2c9f56a67bb358cad25cb2b87b19633
ovaisqureshi57 <oqureshi3@gatech.edu>
2022-10-25 18:27:19 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './Views/Home.js';
 import Contact from "./Views/Contact.js";
-import QuoteGenerator from "./Views/QuoteGenerator.js";
 import Inbox from "./Views/Inbox.js";
 // import Contact from './Views/Contact.js';
 // import Error from './Views/Error.js';
@@ -15,14 +14,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-<<<<<<< Updated upstream
-        {<Route path="/Messages" element = {<Messages1/>}></Route>}
-        {<Route path="/Contact" element={<Contact />} />}
-=======
         <Route path="/Contact" element={<Contact />} />
-        <Route path="/QuoteGenerator" element={<QuoteGenerator />} />
         <Route path="/Inbox" element={<Inbox />} />
->>>>>>> Stashed changes
         {/* <Route path="/*" element={<Error />} /> */}
       </Routes>
     </BrowserRouter>
