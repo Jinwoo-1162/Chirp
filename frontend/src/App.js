ce6e2c1bb7e9de9b9857ea926f4e9e9349c00fb0
18leij2 <jasonleiqi@gmail.com>
2022-11-09 20:57:50 -0500

    commented some code, started revamp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import './App.css';
+// import Home from './Views/Home.js';
+// import Contact from "./Views/Contact.js";
+// // import Contact from './Views/Contact.js';
+// // import Error from './Views/Error.js';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Messages1 from './Views/Messages1.js';
+
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         {<Route path="/Messages" element = {<Messages1/>}></Route>}
+//         {<Route path="/Contact" element={<Contact />} />}
+//         {/* <Route path="/*" element={<Error />} /> */}
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
 import './App.css';
-import Home from './Views/Home.js';
-import Contact from "./Views/Contact.js";
-// import Contact from './Views/Contact.js';
-// import Error from './Views/Error.js';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Messages1 from './Views/Messages1.js';
-
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {<Route path="/Messages" element = {<Messages1/>}></Route>}
-        {<Route path="/Contact" element={<Contact />} />}
-        {/* <Route path="/*" element={<Error />} /> */}
-      </Routes>
-    </BrowserRouter>
-  );
+function App() {    
+  return <div className = 'app'>React App</div>;
 }
-
-export default App;
+  export default App;
