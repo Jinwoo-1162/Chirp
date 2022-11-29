7e74721a774c6b6612d755681b78861210ed04cc
EZ586 <ezhangw@gmail.com>
2022-11-29 15:40:50 -0500

    Updated Posts

--- frontend/src/Components/Feed.js
+++ frontend/src/Components/Feed.js
@@ -56,7 +56,7 @@ function Feed() {
             <Post
                 display="Jason Lei"
                 username="JayC"
-                checkmark={false}
+                checkmark={true}
                 text="I love JoJo. I swear on me mum that I will keep watching until the end!"
                 image="https://media.tenor.com/JTudmgcE8ZIAAAAd/giorno-giovanna-jojo.gif"
                 profile="https://cdn.discordapp.com/avatars/454323721637855232/471a56b78d842a02f46df863cad24551.webp"
@@ -68,6 +68,22 @@ function Feed() {
                 text="Good Book!"
                 image="https://static.wixstatic.com/media/a63a11_2d89fae362414287a9ff739ba21d59d8~mv2.jpg/v1/fit/w_1164,h_789,q_90/a63a11_2d89fae362414287a9ff739ba21d59d8~mv2.webp"
                 profile="https://upload.wikimedia.org/wikipedia/commons/9/97/Ryukin_goldfish_plate.jpg"
+            />
+            <Post
+                display="Eric Zhang"
+                username="EricZ"
+                checkmark={false}
+                text="10/10 Experience, Would never come again"
+                image="https://nique.net/wp-content/uploads/2017/09/westvillage-color.jpg"
+                profile="https://cdn.discordapp.com/avatars/369939851404902400/3cdfe52722ba5c131807fec5ed7e234b.webp?size=128"
+            />
+            <Post
+                display="Eleya Rozel"
+                username="eleya-rozel"
+                checkmark={true}
+                text="as a representative of elon musk im here to sue u"
+                image="https://yt3.ggpht.com/ytc/AMLnZu9t5LdhRVwNIREeSXveYGh4vamfa5V7EeoEAG_A=s900-c-k-c0x00ffffff-no-rj"
+                profile="https://cdn.discordapp.com/avatars/283390809166970880/2710d4bf5f51ec4c0c2006256b94c276.webp?size=240"
             />
 
         </div>
